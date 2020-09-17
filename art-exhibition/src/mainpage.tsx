ffce42c94b6815e2e20155e1172571b894fbebc0
Ingvild Telle Finsås <ingvildtf@dhcp-10-22-74-25.wlan.ntnu.no>
2020-09-17 15:18:05 +0200

    Poetry working with code from piazza (change this). Audio still not working, need help

--- art-exhibition/src/mainpage.tsx
+++ art-exhibition/src/mainpage.tsx
@@ -1,11 +1,11 @@
-//import React from 'react';
-import React, { Component } from 'react'
+import React from 'react';
+//import React, { Component } from 'react'
 import Wrapper from './installation/Installation1';
 import Button from './components/Button';
 import circle from './installation/Installation2';
-//import Text from './poetry/PoetrySvg2';
+import Text from './poetry/PoetrySvg2';
 import AudioPlayer from './music/music2';
-import soundfile from './music/dreams.mp3';
+//import soundfile from './music/dreams.mp3'; //Bare kommentert ut for at kodens kal fungere
 
 
 function Mainpage() {
@@ -14,14 +14,14 @@ function Mainpage() {
         
         <Wrapper >
             <div>{circle}</div>
-            
+            {/* Bare kommentert ut for at koden skal fungere
             <audio>
                 
                 <source src={soundfile} type="audio/mpeg"></source>
-            </audio>
+            </audio> 
             {/*En annen måte å få til audio */}
             <AudioPlayer></AudioPlayer>
-            
+            <Text></Text> {/* Dette er for poetrytekst*/}
             <p>heihei</p>
             <Button>knapp</Button>
         </Wrapper>
