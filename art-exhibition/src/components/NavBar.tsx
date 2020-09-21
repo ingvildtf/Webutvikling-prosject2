95dd43c86a1d59a337beb1c7b45313d6b8b645a3
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2020-09-21 14:47:41 +0200

    adding new files

---
+++ art-exhibition/src/components/NavBar.tsx
@@ -0,0 +1,96 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+//import Button from './Button'; 
+import styled from 'styled-components'
+
+const Wrapper = styled.div `
+    background: #1c2237;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+    position: sticky;
+    top: 0;
+    z-index: 999;
+
+`
+const Container = styled.div `
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    max-width: 1300px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 50px;
+    padding-left: 50px;
+
+    .nav-logo{
+        color: #fff;
+        cusor: pointer;
+        text-decoration: none;
+        font-size: 2rem;
+        padding-top: 15px; 
+        
+    }
+
+    .nav-menu{
+        display: flex;
+        allign-items: center;
+        justify-content: center;
+        text-allign: center;
+    }
+
+
+    .nav-links{
+        display: flex;
+        allign-items: center; 
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        height: 100%; 
+        color: #fff;
+        border-bottom: 2px solid transparent;
+    }
+
+    .nav-links:hover{
+        border-bottom: 2px solid "f00946;
+    }
+    
+`
+
+function Navbar() {
+    //Bruker hooks til å sette staten til knappen 
+    const [click, setClick] = useState(false)
+    
+    const handleClick = () => setClick(!click)
+
+    return (
+        //lager navigasjonsbaren øverst på siden 
+        <Wrapper>
+            <Container className="navbar-container container">
+                <Link to='/'className="nav-logo">
+                    SPACE
+                </Link>
+            
+                {/**Setter navigasjonen til å være drop meny når siden blir under en størrelse */}
+                <ul className={click ? 'nav-menu active': 'nav-menu'}>
+                        <Link to='/home' className="nav-links" >
+                            Home
+                        </Link>
+                    
+                        <Link to='/my-favorites' className="nav-links" >
+                            My Favorites
+                        </Link>
+
+                        <Link to='/settings' className="nav-links">
+                            Settings
+                        </Link>
+                </ul>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default Navbar
