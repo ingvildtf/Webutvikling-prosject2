e136e92153c2f4136e3f9930a224ce998782becc
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2020-09-21 14:43:45 +0200

    laget navigasjonsbar og prøvd å lage installasjon side

--- art-exhibition/src/mainpage.tsx
+++ art-exhibition/src/mainpage.tsx
@@ -1,15 +1,58 @@
 import React from 'react';
-import Wrapper from './installation/Installation1';
-import Button from './components/Button';
+//import Button from './components/Button';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import styled from 'styled-components'
+import Installations from './components/Pages/Installations';
+
+const Wrapper = styled.div `
+
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'PT Sans', sans-serif;
+`;
+
+/*interface WrapperProps{
+    area: string;
+}*/
+
+/*const Text = styled.div <WrapperProps>`
+  grid-area: ${props => props.area};
+`;
+
+const Button = styled.div <WrapperProps>`
+  grid-area: ${props => props.area}; 
+
+`;
+const Navigationbar = styled.div <WrapperProps> `
+    grid-area: ${props => props.area}; 
+`;
+const Header = styled.header <WrapperProps> `
+    grid-area: ${props => props.area}; 
+`;
+
+const Body = styled.body `
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(700px,1fr));
+    grid-template-areas:
+        "btn"
+        "text";
+`;*/
 
 
 function Mainpage() {
     return (
-        <Wrapper >
-            <p>heihei</p>
-            <Button>knapp</Button>
+       <Wrapper>
+            <Router>
+                <NavBar/>
+                <Switch>
+                     <Route exact  path='/home' component={Installations} />
+                     <Route path='/my-favorites' />
+                     <Route path='/settings'/>
+                </Switch>
+            </Router> 
         </Wrapper>
-
     )
 }
 
